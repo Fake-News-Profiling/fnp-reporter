47478090e402c996087273767d0d4f252ae007ab
Josh Hook <joshhook@live.co.uk>
2021-05-12 12:13:47 +0100

    Minor fixes for final project

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="FNP-Reporter",
     version="1.0.0",
-    packages=["fnp-reporter"],
+    packages=["fnpreporter"],
     description="Reporter client for Fake News Profiling",
     long_description=open("README.md").read(),
     install_requires=[
